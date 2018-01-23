cdcb4d8f4dc165448b714edf8a8e7e3d0fd9d1e3
Bryce Frazier and Katie Fujihara <katie@>
2018-01-23 09:40:24 -0800

    add js and jquery functionality. append full name.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function Contact (first, last, phone, email){
   this.lastName = last;
   this.phone = phone;
   this.email = email;
+  this.addresses = [];
 }
 
 function Address (street, city, state, zip){
@@ -17,8 +18,35 @@ Contact.prototype.fullName = function (){
   return this.firstName + " " + this.lastName;
 }
 
+Contact.prototype.phoneEmail = function (){
+  return "Phone: " + this.phone + " and Email: " + this.email;
+}
+
 Address.prototype.fullAddress = function () {
   return this.street + " , " + this.city + " , " + this.state + " " + this.zip;
 }
 
 //front-end logic
+
+$(document).ready(function() {
+  $("#new-contact").submit(function(event) {
+    event.preventDefault();
+    var inputtedFirstName = $(this).find("input#new-first-name").val();
+    var inputtedLastName =  $(this).find("input#new-last-name").val();
+    var inputtedPhoneNumber = $(this).find("input#phoneNumber").val();
+    var inputtedEmail = $(this).find("input#email").val();
+    var newContact = new Contact (inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail);
+
+    $(".newAddress").each(function(){
+    var inputtedStreet = $(this).find("input#street").val();
+    var inputtedCity = $(this).find("input#city").val();
+    var inputtedState = $(this).find("input#state").val();
+    var inputtedZip = $(this).find("input#zip").val();
+
+    var newAddress = new Address (inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+    newContact.addresses.push(newAddress);
+
+    $("ul#contacts").append(newContact.fullName());
+    });
+  });
+});
