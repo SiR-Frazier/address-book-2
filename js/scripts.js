c5b99f59fa7d861f909327fb2e057df0a0a9e1c3
Bryce Frazier and Katie Fujihara <bryce@>
2018-01-23 08:58:53 -0800

    initial commit. add html, css and back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
+//back-end logic
+function Contact (first, last, phone, email){
+  this.firstName = first;
+  this.lastName = last;
+  this.phone = phone;
+  this.email = email;
+}
 
+function Address (street, city, state, zip){
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+}
+
+Contact.prototype.fullName = function (){
+  return this.firstName + " " + this.lastName;
+}
+
+Address.prototype.fullAddress = function () {
+  return this.street + " , " + this.city + " , " + this.state + " " + this.zip;
+}
+
+//front-end logic
